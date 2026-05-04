5c9c8e986f2ef57a5e99ba4687b47bfa5c2f31ff
Ufuk Düşovalı <ufuk_best@hotmail.com>
2026-05-04 16:20:07 +0300

    identity kurulum

---
+++ NotikaIdentityEmail/Context/EmailContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NotikaIdentityEmail.Context
+{
+    public class EmailContext:IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.;initial Catalog=NotikaMailDB;integrated security=true;trust server certificate=true;");
+        }
+    }
+}
