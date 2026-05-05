c4ddf21ac67494a725b2fc4ec0e865e8f70b2229
Ufuk Düşovalı <ufuk_best@hotmail.com>
2026-05-05 14:32:06 +0300

    appuser

---
+++ NotikaIdentityEmail/Controllers/RegisterController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NotikaIdentityEmail.Entities;
+
+namespace NotikaIdentityEmail.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NotikaIdentityEmail/Entities/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Primitives;
+
+namespace NotikaIdentityEmail.Entities
+{
+    public class AppUser:IdentityUser
+    {
+        public string Name  { get; set; }
+        public string Surname  { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? City { get; set; }
+    }
+}
