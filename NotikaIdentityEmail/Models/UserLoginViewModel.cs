dc981b70c280f8c99ab32cfa633a8bde3265d597
Ufuk Düşovalı <ufuk_best@hotmail.com>
2026-05-07 16:27:19 +0300

    inbox  sayfası arayüz yapıldı

--- NotikaIdentityEmail/Models/UserLoginViewModel.cs
+++ NotikaIdentityEmail/Models/UserLoginViewModel.cs
@@ -3,6 +3,6 @@
     public class UserLoginViewModel
     {
         public string Username { get; set; }
-        public string UsernamePassword { get; set; }
+        public string Password { get; set; }
     }
 }
