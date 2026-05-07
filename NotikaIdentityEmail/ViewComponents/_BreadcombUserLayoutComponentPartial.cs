3136f615095176dc694afa7b3686959def6cdfbe
Ufuk Düşovalı <ufuk_best@hotmail.com>
2026-05-07 18:53:25 +0300

    inbox conponent lere bölündü

---
+++ NotikaIdentityEmail/ViewComponents/_BreadcombUserLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotikaIdentityEmail.ViewComponents
+{
+    public class _BreadcombUserLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ NotikaIdentityEmail/ViewComponents/_MainMenuUserLayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NotikaIdentityEmail.ViewComponents
+{
+    public class _MainMenuUserLayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
